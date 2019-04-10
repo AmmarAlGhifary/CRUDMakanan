2740c5716c9f56dd0c6e301c8e680309098f54fa
Ammar Al-Ghifary <ammaralghifary@gmail.com>
2019-04-10 21:15:19 +0700

    updated (tell me if there's a bug)

--- app/src/main/java/com/blogspot/yourfavoritekaisar/crudmakanan/data/remote/ApiClient.java
+++ app/src/main/java/com/blogspot/yourfavoritekaisar/crudmakanan/data/remote/ApiClient.java
@@ -2,17 +2,33 @@ package com.blogspot.yourfavoritekaisar.crudmakanan.data.remote;
 
 import com.blogspot.yourfavoritekaisar.crudmakanan.utils.Constants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
     private static Retrofit retrofit = null;
-
     public static Retrofit getClient(){
+
+
+        // Membuat object logginginterceptor
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+        // Set log level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        // Membuat object httpclient
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        // Menambahkan lginn
+        httpClient.addInterceptor(logging);
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
                 .build();
         return retrofit;
     }
